acd387ebc27c413ea597535d326cfe470801d525
Nikita <nnn.mail44@gmail.com>
2022-09-16 22:00:08 +0300

    inheritance lesson 5 (index_lesson5.php)

--- index_lesson5.php
+++ index_lesson5.php
@@ -1,47 +1,95 @@
 <?php
-    class A
+
+abstract class Model
+{
+    public static function find($id)
     {
-        protected $a;
+        $table = static::tableName();
 
-        public function __construct($a)
+        $sql = 'SELECT * FROM ' . $table . ' WHERE id = :id';
+        echo "$sql <br>";
+        //return $sql;
+    }
+
+    public function delete()
+    {
+        $table = static::tableName();
+
+        $sql = 'DELETE FROM ' . $table . ' WHERE id = :id';
+        echo "$sql <br>";
+        //return $sql;
+    }
+
+    public function create()
+    {
+        $table = static::tableName();
+
+        $data = get_object_vars($this);
+        $property_list = array_keys($data);
+        $values_list = array_map(function($item){
+            return ':' . $item;
+        }, $property_list);
+
+        $sql = 'INSERT ' . $table . ' ('. implode(', ', $property_list) . ') VALUES (' . implode(', ', $values_list) . ')';
+        echo "$sql <br>";
+        //return $sql;
+    }
+
+    public function update()
+    {
+        $table = static::tableName();
+
+        $data = get_object_vars($this);
+        $data_array = array();
+        foreach ($data as $value)
         {
-            $this->setA($a);
+            $value = key($data) . ' = :'. key($data);
+            array_push($data_array, $value);
+            next($data);
         }
 
-        protected function setA($value)
-        {
-            $this->a = $value;
+        if(($key = array_search('id = :id',$data_array)) !== FALSE ) {
+            unset($data_array[$key]);
         }
 
-        public function getA()
-        {
-            return $this->a;
+        $sql = 'UPDATE ' . $table . ' SET '. implode(', ', $data_array) . ' WHERE id = :id';
+        echo "$sql <br>";
+        //return $sql;
+    }
+
+    public function save()
+    {
+        if($this->inserted) {
+            echo "Already exists.. Updating.. <br>";
+            $this->update();
+        } else {
+            echo "Oh, something new.. creating.. <br>";
+            $this->create();
+            $this->inserted = true;
         }
     }
 
-    class B extends A
+    abstract public static function tableName();
+}
+
+final class User extends Model
+{
+    public $id;
+    public $name;
+    public $email;
+    public $inserted = false;
+
+    public static function tableName()
     {
-        private $b;
-
-        public function __construct($a, $b)
-        {
-            parent::__construct($a);
-            $this->setB($b);
-        }
-
-        protected function setB($value)
-        {
-            $this->b = $value;
-        }
-
-        public function getB()
-        {
-            return $this->b;
-        }
+        return 'user';
     }
-    $model1 = new A(0);
-    $model = new B(10, 20);
+}
 
-    var_dump($model->getA());
-    var_dump($model->getB());
-    var_dump($model1->getA());
+//$user = User::find(1);
+
+$user1 = new User();
+/*var_dump($user1->delete());
+var_dump($user1->create());
+var_dump($user1->update());*/
+$user1->save();
+$user1->save();
